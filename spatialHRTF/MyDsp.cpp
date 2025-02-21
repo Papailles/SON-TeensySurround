24aab18b5028d9dcb97a6f27d668dd683a3a5f22
ethgrt <156826375+ethgrt@users.noreply.github.com>
2025-02-21 11:32:23 +0100

    je sais plus

--- spatialHRTF/MyDsp.cpp
+++ spatialHRTF/MyDsp.cpp
@@ -20,6 +20,9 @@ void MyDsp::begin()
     hrtfEngine.addHrir(30,  hrirAz30Left, hrirAz30Right,4, 7, 128);
     hrtfEngine.addHrir(60,  hrirAz60Left, hrirAz60Right, 6, 9, 128);
     hrtfEngine.addHrir(90,  hrirAz90Left, hrirAz90Right, 8, 11, 128);
+
+    // Initialisation de la graine aléatoire (ex. lecture d'une entrée analogique)
+    randomSeed(analogRead(0));
 }
 
 void MyDsp::setFreq(float f)
@@ -37,7 +40,7 @@ void MyDsp::update()
         if(!outBlock[c]) return; // plus de mémoire
     }
 
-    // 1) Générer un bloc mono en float
+    /* 1) Générer un bloc mono en float sinus
     float blockMono[AUDIO_BLOCK_SAMPLES];
     for(int i=0; i<AUDIO_BLOCK_SAMPLES; i++){
         // on utilise la classe Sine
@@ -45,11 +48,20 @@ void MyDsp::update()
         // amplitude par ex. 0.5 si tu veux
         val *= 0.5f;
         blockMono[i] = val;
+    }*/
+
+    // Générer un bloc de bruit blanc en float
+    float blockMono[AUDIO_BLOCK_SAMPLES];
+    for (int i = 0; i < AUDIO_BLOCK_SAMPLES; i++) {
+        // Génère un bruit blanc dans la plage [-1, +1]
+        float noiseSample = ((float)random(-32768, 32767)) / 32768.0f;
+        // Appliquer un facteur d'amplitude (ici 0.5 par exemple)
+        blockMono[i] = noiseSample * 0.5f;
     }
 
     // 2) Sélection de la HRIR
     static int angle = 0;
-    angle+=0.1;
+    angle+=0.0001;
     if(angle>90) angle=0;
     auto selHrir = hrtfEngine.getHrir(angle);
 
